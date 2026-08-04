9ecc5857812090a748e309ff5bd9c577c5b47937
Natanael Fernando Gatti Brentano <natanaelfernando@outlook.com>
2026-08-04 17:57:56 -0300

    refactor: clean up layout structure and remove redundant text blocks in apneia-e-ronco page

--- app/apneia-e-ronco/page.jsx
+++ app/apneia-e-ronco/page.jsx
@@ -66,94 +66,48 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <main id="wp--skip-link--target" class="wp-block-group is-layout-flow wp-container-core-group-is-layout-36bb09e9 wp-block-group-is-layout-flow" style="margin-top:0">
     <div class="entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">
 <div class="wp-block-columns alignwide are-vertically-aligned-center is-style-default is-layout-flex wp-container-core-columns-is-layout-3b811c60 wp-block-columns-is-layout-flex">
-<div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px">
-<figure class="wp-block-image size-full"><img data-od-unknown-tag data-od-xpath="/HTML/BODY/DIV[@class=&apos;wp-site-blocks&apos;]/*[2][self::MAIN]/*[1][self::DIV]/*[1][self::DIV]/*[1][self::DIV]/*[1][self::FIGURE]/*[1][self::IMG]" data-dominant-color="987f77" data-has-transparency="false" fetchpriority="high" decoding="async" width="1440" height="2560" sizes="(max-width: 310px) 100vw, 310px" src="/wp-content/uploads/2025/08/DSC_4829-edited-scaled.avif" alt="DSC 4829 edited scaled - Tratamento de Ronco e Apneia do Sono | CPAP – Lajeado e Vale do Taquari" class="wp-image-97 not-transparent" style="--dominant-color: #987f77; aspect-ratio:3/4;object-fit:cover" title="Apneia e Ronco 1" srcset="/wp-content/uploads/2025/08/DSC_4829-edited-scaled.avif 1440w, /wp-content/uploads/2025/08/DSC_4829-edited-169x300.avif 169w, /wp-content/uploads/2025/08/DSC_4829-edited-576x1024.avif 576w, /wp-content/uploads/2025/08/DSC_4829-edited-768x1365.avif 768w, /wp-content/uploads/2025/08/DSC_4829-edited-864x1536.avif 864w, /wp-content/uploads/2025/08/DSC_4829-edited-1152x2048.avif 1152w" /></figure>
+<div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow">
+<figure class="wp-block-image alignfull size-full"><img data-od-unknown-tag data-od-xpath="/HTML/BODY/DIV[@class=&apos;wp-site-blocks&apos;]/*[2][self::MAIN]/*[1][self::DIV]/*[1][self::DIV]/*[1][self::DIV]/*[1][self::FIGURE]/*[1][self::IMG]" data-dominant-color="987f77" data-has-transparency="false" fetchpriority="high" decoding="async" width="1440" height="2560" sizes="(max-width: 720px) 100vw, 720px" src="/wp-content/uploads/2025/08/DSC_4829-edited-scaled.avif" alt="DSC 4829 edited scaled - Tratamento de Ronco e Apneia do Sono | CPAP – Lajeado e Vale do Taquari" class="wp-image-97 not-transparent" style="--dominant-color: #987f77;" title="Apneia e Ronco 1" srcset="/wp-content/uploads/2025/08/DSC_4829-edited-scaled.avif 1440w, /wp-content/uploads/2025/08/DSC_4829-edited-169x300.avif 169w, /wp-content/uploads/2025/08/DSC_4829-edited-576x1024.avif 576w, /wp-content/uploads/2025/08/DSC_4829-edited-768x1365.avif 768w, /wp-content/uploads/2025/08/DSC_4829-edited-864x1536.avif 864w, /wp-content/uploads/2025/08/DSC_4829-edited-1152x2048.avif 1152w" /></figure>
+</div>
 
-
-
-<div class="wp-block-column is-vertically-aligned-center is-style-default has-global-padding is-content-justification-center is-layout-constrained wp-container-core-column-is-layout-db5259ab wp-block-column-is-layout-constrained" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+<div class="wp-block-column is-vertically-aligned-center is-style-default is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:50%">
 <h1 class="wp-block-heading alignfull has-large-font-size"><strong><strong>Apneia e Ronco</strong></strong></h1>
 
-
-
-<div class="wp-block-columns alignfull is-layout-flex wp-container-core-columns-is-layout-3b811c60 wp-block-columns-is-layout-flex">
-<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:100%">
 <p class="has-text-align-justify wp-block-paragraph">A fisioterapia do sono é uma área em expansão que atua na identificação, intervenção e acompanhamento de pacientes com <strong>distúrbios respiratórios relacionados ao sono</strong>, como o <strong>ronco</strong> e a <strong>apneia obstrutiva do sono (SAOS)</strong>. Com especialização e experiência clínica, ofereço cuidados integrados para promover noites mais tranquilas e uma saúde global mais equilibrada.</p>
-</div>
-</div>
-
-
 
 <h4 class="wp-block-heading alignfull">💨 Tratamento e Manejo da Apneia Obstrutiva do Sono (SAOS)</h4>
 
-
-
+<div class="wp-block-group has-global-padding is-content-justification-center is-layout-constrained wp-block-group-is-layout-constrained">
 <ul class="wp-block-list">
 <li>Avaliação funcional da respiração durante o sono</li>
-
-
-
 <li>Orientações sobre higiene do sono e impacto da apneia na saúde cardiovascular</li>
-
-
-
 <li>Prescrição e acompanhamento do tratamento com <strong>CPAP</strong> e <strong>Binível (BiPAP)</strong></li>
-
-
-
 <li>Suporte completo na <strong>adaptação aos equipamentos</strong>, garantindo conforto e aderência ao tratamento</li>
 </ul>
-
-
+</div>
 
 <h4 class="wp-block-heading alignfull">🛠️ Assistência Técnica e Personalização</h4>
 
-
-
+<div class="wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 <ul class="wp-block-list">
 <li><strong>Teste de máscaras</strong> para escolher o modelo ideal, conforme anatomia e padrão respiratório</li>
-
-
-
 <li><strong>Reposição de materiais para CPAP</strong>, como filtros, tubos e almofadas</li>
-
-
-
 <li><strong>Aluguel de CPAP</strong>, ideal para quem está iniciando ou fazendo teste terapêutico</li>
-
-
-
 <li>Monitoramento do uso e eficácia do equipamento com suporte contínuo</li>
 </ul>
-
-
+</div>
 
 <h4 class="wp-block-heading alignfull">🧘‍♀️ Benefícios esperados</h4>
 
-
-
+<div class="wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 <ul class="wp-block-list">
 <li>Redução da sonolência diurna e melhora da concentração</li>
-
-
-
 <li>Diminuição do ronco e pausas respiratórias</li>
-
-
-
 <li>Melhora da oxigenação durante o sono</li>
-
-
-
 <li>Mais energia, disposição e bem-estar nas atividades diárias</li>
-
-
-
-<div class="wp-block-columns alignfull is-layout-flex wp-container-core-columns-is-layout-3b811c60 wp-block-columns-is-layout-flex">
-<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
-<p class="has-text-align-justify wp-block-paragraph">A fisioterapia do sono é uma área em expansão que atua na identificação, intervenção e acompanhamento de pacientes com <strong>distúrbios respiratórios relacionados ao sono</strong>, como o <strong>ronco</strong> e a <strong>apneia obstrutiva do sono (SAOS)</strong>. Com especialização e experiência clínica, ofereço cuidados integrados para promover noites mais tranquilas e uma saúde global mais equilibrada.</p>
+</ul>
 </div>
-</div>
+
 </div>
 </div>
 </div>
